f10d94f4805ce87d5f744261726bc6c7113c9418
Fasika Dagnachew <fasika1515@gmail.com>
2024-11-14 17:45:09 +0300

    fixed filename bug in blogs hero

--- app/media/blogs/page.tsx
+++ app/media/blogs/page.tsx
@@ -7,7 +7,7 @@ export default function page() {
   return (
     <div className='relative w-full min-h-screen'>
         <ParticlesBg color="#78e0f4" amount={100} />
-        <Hero img='/images/pageshero/blogs.jpg' title='Blogs' />
+        <Hero img='/images/pageshero/blogs.JPG' title='Blogs' />
         <BlogList />
     </div>
   )
