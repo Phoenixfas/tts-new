bc655e78e7154513dd8686df0f5b6d8f7acbf234
Fasika Dagnachew <fasika1515@gmail.com>
2024-12-07 05:05:59 +0300

    sectors image

--- data/sectors.ts
+++ data/sectors.ts
@@ -3,19 +3,19 @@ const sectors = [
         id: "1",
         name: "Agri Tech",
         description: "Dive into the future of farming with Agri Tech, where innovation meets sustainability to revolutionize food production and enhance global food security.",
-        image: "/images/sectors/agri-tech.jpg"
+        image: "/images/sectors/agri.jpg"
     },
     {
         id: "2",
         name: "Fintech",
         description: "Unlock the potential of your financial future with Fintech, where cutting-edge technology transforms banking, investing, and personal finance into seamless and accessible experiences.",
-        image: "/images/sectors/fintech.png"
+        image: "/images/sectors/fin.jpg"
     },
     {
         id: "3",
         name: "Health Tech",
         description: "Explore the groundbreaking advancements in Health Tech, where innovative solutions are reshaping patient care, enhancing diagnostics, and making healthcare more personalized than ever.",
-        image: "/images/sectors/health-tech.jpg"
+        image: "/images/sectors/health.jpg"
     },
     {
         id: "4",
@@ -27,19 +27,19 @@ const sectors = [
         id: "5",
         name: "Cyber Security",
         description: "Protect what matters most by delving into Cyber Security, where robust defenses are built to safeguard your digital life against evolving threats and vulnerabilities.",
-        image: "/images/sectors/cyber-security.jpg"
+        image: "/images/sectors/cyber.jpg"
     },
     {
         id: "6",
         name: "E-Commerce",
         description: "Experience the dynamic landscape of E-Commerce, where shopping is reimagined through technology, offering convenience, variety, and personalization at your fingertips.",
-        image: "/images/sectors/e-commerce.jpg"
+        image: "/images/sectors/ecom.jpg"
     },
     {
         id: "7",
         name: "Smart City",
         description: "Join the movement towards Smart Cities, where technology integrates seamlessly with urban life, enhancing sustainability, mobility, and the overall quality of living for all residents.",
-        image: "/images/sectors/smart-city.jpg"
+        image: "/images/sectors/city.jpg"
     },
 ]
 
